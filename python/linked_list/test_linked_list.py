90ad6d3412f9d1681bf96df9cdf657f8adb1c386
sam <samuelclark907@gmail.com>
2020-12-22 20:42:17 -0900

    chall 06 done tests pass

--- python/linked_list/test_linked_list.py
+++ python/linked_list/test_linked_list.py
@@ -2,6 +2,7 @@ from linked_list.linked_list import LinkedList, Node
 import pytest
 
 
+# Code Challenge 05 tests
 
 a = Node(2)
 b = LinkedList()
@@ -16,18 +17,44 @@ def testtwo():
     assert c.head == 1
 
 
-def testthree():
+def test_insert1():
     c.insert(3)
     assert c.head.value == 3
 
 
-def testfour():
+def test_insert2():
     c.insert(5)
     assert c.head.value == 5
 
 
-def testfive():
+def test_includes():
     assert c.includes(5) == True
 
+# Code Challenge 06 tests
 
+
+def test_append():
+    newone_linked = LinkedList(Node(1))
+
+    newone_linked.append(7)
+    assert newone_linked.includes(7)
+
+
+def test_insertBefore():
+    newone_linked = LinkedList(Node(1))
+
+    newone_linked.insertBefore(1,2)
+    assert newone_linked.head.value == 2
+    assert newone_linked.head.next.value == 1
+
+
+def test_insertAfter():
+    newone_linked = LinkedList(Node(1))
+    newone_linked.insertAfter(1,3)
+    assert newone_linked.head.value == 1
+    assert newone_linked.head.next.value == 3
+    
+    
+
+    
 
