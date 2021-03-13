5a3a8c00d9b662d06060b9ccc0e0025faf7ae6ff
sam <samuelclark907@gmail.com>
2021-03-13 13:21:29 -0900

    not sure

--- python/quick_sort/test_quick_sort.py
+++ python/quick_sort/test_quick_sort.py
@@ -2,8 +2,8 @@ import pytest
 from quick_sort import QuickSort, Partition
 
 def test_quick_sort():
-    lst = [6,5,4,3,2,1]
+    lst = [6,5,10,3,4,2,1]
     n = len(lst)
     actual = QuickSort(lst, 0, n-1)
-    expected = [1,2,3,4,5,6]
+    expected = [1,2,3,4,5,6,10]
     assert actual == expected
